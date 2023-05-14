43677b6dfe432b5d5914d25dab82a112fcb386d4
Ray Luna <leon.luna.ray@gmail.com>
2023-05-13 23:41:10 -0700

    install vite.js, upgrade to react 18

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,11 @@
 // This code will contain the API call rather than having it on the App.js
 import axios from 'axios';
-// The create method will create a new axios instance with prepopulated data
+
+const apiKey = import.meta.env.VITE_APP_UNSPLASH_API_KEY;
+
 export default axios.create({
   baseURL: 'https://api.unsplash.com/',
   headers: {
-    Authorization: 'Client-ID 4X1RWjcoO5BPB_6WVEyaGmtWG9EmKAyom5uwjBoQ2Is',
+    Authorization: `Client-ID ${apiKey}`,
   },
 });
